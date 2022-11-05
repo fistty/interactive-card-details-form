3d5bcf50b1858fad3709a0c3dd643029d27e6a2f
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2022-11-05 14:52:25 +0100

    Update handleSlice.js

--- src/handleSlice.js
+++ src/handleSlice.js
@@ -1,6 +1,6 @@
 export const handleSlice = (e, num) => {
-  if (e.target.value.length > e.target.maxLength) {
-    e.target.value = e.target.value.slice(0, num || e.target.maxLength);
+  if (e.target.value.length > num) {
+    e.target.value = e.target.value.slice(0, num);
   }
   e.target.value = e.target.value.replace(/\D/g, "");
 };
